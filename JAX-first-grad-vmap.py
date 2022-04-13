978cfda7ae6c0c0a95b5447e97ba311c833c3b40
Jean-Eric Campagne <campagne@lal.in2p3.fr>
2022-04-13 15:51:50 +0200

    Jax-first-grad-vmap: ajout de commentaires/exercices/takeaway

--- JAX-first-grad-vmap.py
+++ JAX-first-grad-vmap.py
@@ -34,48 +34,86 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 mpl.rc('image', cmap='jet')
 mpl.rcParams['font.size'] = 18
-mpl.rcParams["font.family"] = "Times New Roman"
+
+
 # -
 
-if jax.__version__ < '0.2.26':
-    clear_cache = jax.interpreters.xla._xla_callable.cache_clear
-else:
-    clear_cache = jax._src.dispatch._xla_callable.cache_clear
+# ## Attention: Il y a (au moins) 2 pratiques en vigueur: 
+# - soit (numpy -> np, jax.numpy -> jnp) 
+# - soit (numpy -> onp, jax.numpy -> np)
 
-
-# # Example de l'usage de l'auto-diff et vectorisation
+# # Example de l'usage de l'auto-diff et la vectorisation
 
 def f(x):
     return jnp.exp(-x*0.5)*jnp.sin(x)
 
 
+# +
+# Expressions Exactes de f'(x) et f''(x)
 def fp(x):
     return jnp.exp(-x*0.5)*(2.*jnp.cos(x)-jnp.sin(x))/2.
 def fpp(x):
     return -jnp.exp(-x*0.5)*(4.*jnp.cos(x)+3.*jnp.sin(x))/4.
 
+# Expressions numériques centrées approchées de f'(x) et f''(x)
+def fpnum(x,h=0.1):
+    return (f(x+h)-f(x-h))/(2*h)
+def fppnum(x,h=0.1):
+    return (f(x+h)-2*f(x)+f(x-h))/h**2
 
-jfp  = grad(f)
-jfpp = grad(grad(f))
 
-# +
-#jax.make_jaxpr(jfp)(0.)
 # -
 
-jfp = vmap(grad(f))          # besion de vectorisation pour appeler avec un array
-jfpp = vmap(grad(grad(f)))
+# # Différéntiations successives
 
-fig,_ = plt.subplots(figsize=(8,8))
+jfp  = grad(f)             # grad pour gradient
+jfpp = grad(grad(f))
+
+# ## Voir que fait JAX quand il produit le code...
+
+jax.make_jaxpr(jfp)(0.)
+
+# ### prenez un papier et un crayon et vérifier que ce code est correcte :) 
+
+# ## La vectorisation est utile pour appeler la fonction avec un array (nb. et d'autres structures)
+
+jfp  = vmap(grad(f))
+jfpp = vmap(grad(grad(f)))
+#jfjacobien = vmap(jacfwd(f))
+#jfhessien  = vmap(hessian(f))
+
+fig,_ = plt.subplots(figsize=(8,9))
 x=jnp.arange(0,10,0.1)
 plt.plot(x,f(x), label="$f(x)$")
-plt.plot(x,jfp(x), lw=3,label="$f^{\ \prime}(x)$ (JAX)")
-plt.plot(x,fp(x),ls="--",c='k', label="$f^{\ \prime}(x)$")
-plt.plot(x,jfpp(x), lw=3,label="$f^{\ \prime\prime}(x)$ (JAX)")
-plt.plot(x,fpp(x),ls="--", c='purple',label="$f^{\ \prime\prime}(x)$")
+plt.scatter(x,fpnum(x), label="$f^{\prime}(x)$ (Num.)")
+plt.plot(x,jfp(x), lw=3,label="$f^{\prime}(x)$ (JAX)")
+plt.plot(x,fp(x),ls="--",c='k', label="$f^{\ \prime}(x)$ (True)")
+#plt.plot(x,jfjacobien(x),ls='-.', lw=3, label='jacobien')
+plt.scatter(x,fppnum(x), label="$f^{\prime\prime}(x)$ (Num.)")
+plt.plot(x,jfpp(x), lw=3,label="$f^{\prime\prime}(x)$ (JAX)")
+plt.plot(x,fpp(x),ls="--", c='purple',label="$f^{\ \prime\prime}(x)$ (True)")
+#plt.plot(x,jfhessien(x), ls='-.', lw=3, label='hessian')
 plt.grid()
 plt.xlabel("$x$")
-plt.legend();
+plt.legend(loc="upper right");
 
+# ### Zoom: => Q: à quoi sert l'auto-différentiation? 
+
+x=jnp.arange(0,10,0.1)
+plt.scatter(x,fppnum(x)-fpp(x),s=10, c="r", label="Num.-True")
+plt.plot(x,jfpp(x)-fpp(x),lw=3,c="g",  label="Jax-True")
+plt.grid()
+plt.xlabel("$x$")
+plt.title("$f^{\prime\prime}(x)$")
+plt.legend(loc="upper right");
+
+x=jnp.arange(0,10,0.1)
+jnp.max(jnp.abs(jfpp(x)-fpp(x)))
+
+
+# vous pouvez essayer d'utiliser le *jacobien* et le *hessien* ...
+
+# ## La différentiation est par défaut sur le 1er argument de la fonction mais cela n'est pas exclusif
 
 # +
 def func(x, y):
@@ -89,7 +127,9 @@ print(grad(func, argnums=(0, 1))(3., 4.))  # (8., 6.)  # ici on fait en mm temps
 
 # -
 
-# # Un petit fit par minimisation de la MSE (Mean Squared Error)
+# essayer avec une autre fonction...
+
+# # Exemple via la minimisation de la Mean Squared Error (MSE)
 
 # +
 def model(p, x):
@@ -100,33 +140,45 @@ def loss_fun(p, xi, yi):
     return jnp.mean( (yhat - yi)**2 )
 
 
+# -
+
+# jnp.exp, jnp.sin ... sont vectorisees donc `model` et `loss_fun` n'ont pas besoin d'un vmap
+#
+
 # +
 # dataset
-ptrue = np.array([0.5,1])
+ptrue = jnp.array([0.5,1])
 
-xin = np.arange(0,10,1.)
+xin = jnp.arange(0,10,1.)
 yerr = 0.05
-# jnp.exp, jnp.sin ... sont vectorisees donc "model" n'a pas besoin d'unvmap
 # jax.random.PRNGKey(seed) gestion des randoms numbers
 y_true =  model(ptrue, xin)
 yin = y_true + yerr*jax.random.normal(jax.random.PRNGKey(42),shape=xin.shape)
 # -
+
+xin
+
+# on peut directement utiliser les DeviceArray de JAX dans matplotlib
 
 plt.errorbar(xin,yin,yerr=yerr, fmt='o', linewidth=2, capsize=0, c='k', label="data");
 plt.plot(xin,y_true, label='True (noiseless)')
 plt.legend();
 
 
-# ### Descente de Gradient
+# ## Descente de Gradient
 
 ## @jit
 def gradient_descent_step(p, xi, yi, lr=0.1):
     return p - lr * jax.grad(loss_fun)(p, xi, yi)
 
 
+# nb. nous verrons le pourquoi du `@jit` un peu plus loin
+
+# Un minizer basique (version plus JAX/LAX-like à la fin)
 def minimzer(loss_fun, x_data, y_data, par_init, method, verbose=True):
     p_cur = par_init
     new_loss=jnp.inf
+    
     for t in range(5000):
    
         if (t % 100 == 0) and verbose:
@@ -136,7 +188,7 @@ def minimzer(loss_fun, x_data, y_data, par_init, method, verbose=True):
         new_p = method(p_cur, x_data,y_data)
         new_loss = loss_fun(new_p, x_data,y_data)
 
-        if np.abs(new_loss-old_loss) < 1e-9:
+        if jnp.abs(new_loss-old_loss) < 1e-9:
             print(f"Converged after {t} epochs: p = {new_p}, loss = {new_loss}")
             break
 
@@ -156,7 +208,7 @@ plt.plot(x_val,model(ptrue, x_val),label="true noiseless")
 plt.plot(x_val,model(par_mini_GD, x_val) ,lw=3,label="GD fit")
 plt.legend();
 
-# ### Newton => hessien
+# ## Méthod  de Newton => usage du hessien (cf. méthode d'ordre 2, alors que GD : méthode d'ordre 1)
 
 # +
 gLoss = lambda p,xi,yi: jacfwd(loss_fun)(p,xi,yi)
@@ -247,12 +299,21 @@ def show_example(structured):
           structured, flat, tree, unflattened))
 
 
-
 show_example(my_params)
 
+
+# # Takeaway message:
+# - JAX ne nécessite pas l'apprentissage d'un nouveau langage de programmation 
+# - Avec `jax.numpy` il est (presque) aussi simple d'utiliser les JAX DeviceArray en lieu et place des Numpy array
+# - L'autofifferentiation: c'est pas compliqué (`grad`, `jacfwd`, `hessian`)
+# - `jit` permet d'accélérer le code (compilation au vol)
+# - `vmap` permet de vectoriser l'appel de fonctions (nb. il existe `pmap` pour la paraléllisation sur plusieurs devices)
+# - On peut utiliser (grad/vmap...) ses structures autres que les arrays: tuple, liste, dico et user PyTree
+
+# # Exercice:
+# Reprener les méthodes `model(p, x)`, `loss_fun(p, xi, yi)`,  `oneStepNewton(p,xi,yi,lr=0.1)` en changeant la sturture des paramètres `p` (le default est `jnp.array`): ex utiliser un `dictionnary` et/ou une classe sur le modèle de `Params`. 
+
 # # EXTRA qd on aura pratiqué l'exemple de la fratale de Julia
-
-
 
 def minimzer_bis(loss_fun, x_data, y_data, par_init, method, maxiter=5000, loss_diff=1e-9):
 
